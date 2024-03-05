af913b37e0100aaaae1225d240b4061c59f7d375
Degef Sium <Degei411233@outlook.com>
2024-03-05 21:32:19 +0400

    final update 05 Mar 2024

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-e8g6hg7d-&lt3mbs_eg=5qjcr(n()2=8!vtyxq)g*r07j%=iex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
